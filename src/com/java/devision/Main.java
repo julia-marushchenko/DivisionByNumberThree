5fc8f16615fad5f8a831843b9ce8c029ca4b5b81
Julia Marushchenko <marushchenko.yulia@gmail.com>
2024-08-01 20:55:00 +0200

    Add files via upload
    Commiting the solution of the task.

---
+++ src/com/java/devision/Main.java
@@ -0,0 +1,23 @@
+package com.java.devision;
+
+public class Main {
+	
+	// Number 904. How many times is divided by 3.
+	
+	public static void main(String [] str){
+		
+		int count = 0;
+		int number = 0;
+		System.out.print("Numbers are: ");
+		for(int index = 0; index < 905; index++) {
+			number++;
+			if(number % 3 == 0) {
+				count++;
+				System.out.print(number + "; ");
+			}
+		}
+		System.out.println("\nAmount of timems from 0 till 904 are met numbers that can be divided by "
+				+ "number 3 is " + count);
+	}
+
+}
